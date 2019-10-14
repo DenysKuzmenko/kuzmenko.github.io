40b5e9c42d362ac867d51b605550a744ee06a8a9
Denys <Denfalcon@gmail.com>
2019-10-14 19:43:42 +0200

    test

---
+++ js/custom.js
@@ -0,0 +1,90 @@
+// JavaScript Document
+jQuery.noConflict();
+jQuery(document).ready(function () {
+
+    'use strict';
+
+	var slid = jQuery('#next-slider');
+		
+	slid.owlCarousel({
+		items: 1,
+		autoplay: true,
+		autoplayTimeout: 6000,
+		autoplayHoverPause: false,
+		smartSpeed: 1000,
+		dotsSpeed: 1000,
+		loop: true,
+		nav: true,
+		navText: ['', ''],
+		dots: false,
+		mouseDrag: true,
+		margin: 30,
+		stagePadding: 0,
+		autoWidth: false,
+		responsiveClass: true,
+		responsive:{
+			0:{
+				items: 1,
+				dots: true,
+				mouseDrag: true,
+				autoplay: false
+			},
+			479:{
+				items: 1,
+				mouseDrag: true
+			},
+			767:{
+				items: 1,
+				mouseDrag: true
+			},
+			991:{
+				items: 1,
+				mouseDrag: true
+			},
+			1099:{
+				items: 1,
+				mouseDrag: true
+			}
+		}
+	});
+
+	var slid = jQuery('#client-slider');
+		
+	slid.owlCarousel({
+	loop:true,
+    margin:10,
+    nav:true,
+    dotsSpeed: 500,
+    navText: ['',''],
+	responsive:{
+			0:{
+				items: 1,
+				dots: true,
+				mouseDrag: true,
+				autoplay: false
+			},
+			479:{
+				items: 1,
+				mouseDrag: true
+			},
+			767:{
+				items: 2,
+				mouseDrag: true
+			},
+			991:{
+				items: 3,
+				mouseDrag: true
+			},
+			1099:{
+				items: 4,
+				mouseDrag: true
+			}
+		}
+	});
+	
+	jQuery('.btn_menu').click(function(event){
+				jQuery('.nav-menu ul').toggle('slow');
+		});
+	
+	
+	});
